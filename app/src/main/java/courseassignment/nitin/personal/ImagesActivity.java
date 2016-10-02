ce7b23f3ec883df7b95580f0adf3acb869033350
Nitin Varun <nitin13070@iiitd.ac.in>
2016-10-02 23:50:06 +0530

    Added External or Internal Storage

--- app/src/main/java/courseassignment/nitin/personal/ImagesActivity.java
+++ app/src/main/java/courseassignment/nitin/personal/ImagesActivity.java
@@ -1,14 +1,115 @@
 package courseassignment.nitin.personal;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ImagesActivity extends AppCompatActivity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    String mCurrentPhotoPath;
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            Bitmap imageBitmap = BitmapFactory.decodeFile(mCurrentPhotoPath);
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            View dialogView = getLayoutInflater().inflate(R.layout.show_image_dialog,null);
+            ImageView imageView = (ImageView) dialogView.findViewById(R.id.show_image_view);
+            imageView.setImageBitmap(imageBitmap);
+            builder.setView(dialogView);
+            builder.create().show();
+
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_images);
         getSupportActionBar().setTitle(getString(R.string.images_title));
+    }
+
+    public void addImage(View view){
+
+    }
+
+    public void saveImage(View view){
+
+
+                Toast.makeText(this,"Internal",Toast.LENGTH_SHORT).show();
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                if(takePictureIntent.resolveActivity(getPackageManager()) != null){
+
+                    File photoFile = null;
+
+                    try {
+                        if(view.getId() == R.id.save_int_btn){
+                            photoFile = createInternalImageFile();
+                        }
+                        else if(view.getId() == R.id.save_ext_btn){
+                            photoFile = createExternalImageFile();
+                        }
+
+                    } catch (IOException ex){
+
+                    }
+
+                    if(photoFile != null){
+                        Uri photoURI = FileProvider.getUriForFile(this, "com.example.android.fileprovider", photoFile);
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                        startActivityForResult(takePictureIntent,REQUEST_IMAGE_CAPTURE);
+                    }
+
+                }
+
+    }
+
+
+
+    private File createInternalImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private File createExternalImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 }
